0879724dce41f32427409394467038aec5551374
chittaranjan2020 <84212346+chittaranjan2020@users.noreply.github.com>
2021-06-01 13:02:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-// ©Avishkarpatil [ Telegram ]
+// ©Chittaranjan [ Telegram ]
 // usage index.php?c=0-9-129
 // Don't Change Any Thing !
 
 $input = $_GET["c"];
 if (!$input){
-   exit("<h3>Channel ID not found <br><br>You Entered Worng ID or Not Entered ID Here</h3><br><h4> Use Correct Format ➸ <code>https://zee.avipatilpro.repl.co/?c=CHANNEL_ID_HERE</code> <br><br><br> <h4> ➤ Created by <a href='https://github.com/avipatilpro'>Avi Patil</a></h4> ");
+   exit("<h3>Channel ID not found <br><br>You Entered Worng ID or Not Entered ID Here</h3><br><h4> Use Correct Format ➸ <code>https://zee.avipatilpro.repl.co/?c=CHANNEL_ID_HERE</code> <br><br><br> <h4> ➤ Created by <a href='https://github.com/chittaranjan2020'>Chittaranjan Maharana</a></h4> ");
   
 }
 $channel_meta = JsonfromURI("https://catalogapi.zee5.com/v1/channel/${input}");
